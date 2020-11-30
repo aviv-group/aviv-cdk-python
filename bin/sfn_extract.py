7bab24ba53d0d923b226687c9b6c7184acbd3116
Jules Clement <jules.clement@aviv-group.com>
2020-11-30 06:04:24 +0100

    fix for CDK-specific Parameters/Rules

--- bin/sfn_extract.py
+++ bin/sfn_extract.py
@@ -14,9 +14,17 @@ def cli(template: str):
         # Extract and generate CFN template parameters
         tpl_raw = json.load(f)
         params = dict()
+
+        # Cleanup CDK specific stuff
+        if 'Rules' in tpl_raw and 'CheckBootstrapVersion' in tpl_raw['Rules']:
+            del tpl_raw['Rules']['CheckBootstrapVersion']
+        if 'Parameters' in tpl_raw and 'BootstrapVersion' in tpl_raw['Parameters']:
+            del tpl_raw['Parameters']['BootstrapVersion']
+
         for k, v in tpl_raw['Parameters'].items():
-            params[k] = v['Description'].split('"')[1]
-            if str(k).find("S3Bucket"):
+            if 'Description' in v and v['Description'].find('"') >= 0:
+                params[k] = v['Description'].split('"')[1]
+            if str(k).find("S3Bucket") >= 0:
                 params[k] = "cdk.out/asset||" + params[k]
         # Parse CFN template
         tpl = aws_parsecf.loads_json(json.dumps(tpl_raw), default_region='eu-west-1', parameters=params)

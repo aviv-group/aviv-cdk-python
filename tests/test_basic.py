c0a678736ed68cb9ab6afa705107832a8fadbf61
Jules Clement <jules.clement@aviv-group.com>
2021-01-19 11:49:39 +0100

    add basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,17 @@
 import pytest
 import aviv_cdk
 
+from aws_cdk import core
 
+app = core.App()
+stack = core.Stack(app, 'stack')
 
 class TestAvivCdk:
     def test_version(self):
         assert aviv_cdk.__version__
 
+    def test_loadpipeline(self):
+        import aviv_cdk.pipelines as ap
+        p = ap.Pipeline(stack, 'ptest')
+
+        assert isinstance(p, ap.Pipeline)
